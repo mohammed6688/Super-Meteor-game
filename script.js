98e78a751ace0285fb609debabb313671f82b0b5
mohammed6688 <mohammed668800.ma@gmail.com>
2022-01-06 08:54:46 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var aud = document.getElementById("audio");
 var button = document.getElementById("button");
+var setting = document.getElementById("setting");
 
-var audio = new Audio('music_bg.mp3');
+var audio = new Audio('assets/music_bg.mp3');
 audio.volume = 0.4;
 aud.addEventListener('click', e => {
     if (audio.paused) {
@@ -11,3 +12,10 @@ aud.addEventListener('click', e => {
     }
 });
 
+button.addEventListener('click', e => {
+    console.log("clicked");
+});
+
+setting.addEventListener('click', e => {
+    console.log("setting clicked");
+});
