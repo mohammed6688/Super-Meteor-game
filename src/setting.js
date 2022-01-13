5de9027629b0c767adf6dc53cbbf42aefe756b85
Ayman <ayman.ibra7im.ahmed@gmail.com>
2022-01-13 21:52:53 +0200

    final modification on sitting page

--- src/setting.js
+++ src/setting.js
@@ -4,18 +4,18 @@ var hard = document.getElementById("hard");
 
 let level=null;
 easy.addEventListener('click', e => {
-    //put code her
+   
     window.localStorage.setItem("level",0);
     window.open("index2.html", "_self");
 });
 medium.addEventListener('click', e => {
-    //put code here
+    
     //window.localStorage.setItem("level","medium");
     window.localStorage.setItem("level",1);
     window.open("index2.html", "_self");
 });
 hard.addEventListener('click', e => {
-    //put code here
+    
     window.localStorage.setItem("level",2);
     window.open("index2.html", "_self");
 });
