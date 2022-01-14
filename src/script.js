8e0956bbfedc138ebdabf6315d4dfdda3a128b9a
Alzahraa Eid <alzahraaeid62@gmail.com>
2022-01-14 14:41:01 +0200

    adding settings

--- src/script.js
+++ src/script.js
@@ -1,7 +1,67 @@
 var button = document.getElementById("button");
+var settings = document.getElementById("img_setting")
+var sub_menu = document.getElementById("sub_menu")
+var mainMusic = document.getElementById("mainMusic")
+var slider_song1 = document.getElementById("song1")
+var img_song1 = document.getElementById("img_unmute1")
 
+slider_song1.value = JSON.parse(localStorage.getItem('sliderVal'))
+localStorage.setItem('pre_sliderVal',slider_song1.value)
+if(slider_song1.value == 0)
+{
+    img_song1.setAttribute('src','assets/mute.png')
+}
+else
+{
+    img_song1.setAttribute('src','assets/voice.png')
+}
+// var flag_mute_unmute =1;
+mainMusic.autoplay = true
+mainMusic.loop = true
 button.addEventListener('click', e => {
-    //console.log("clicked");
+    
     window.open("index2.html", "_self");
+
+});
+
+settings.addEventListener('click', e => {
+    sub_menu.style.display = "block";
+
+});
+
+slider_song1.addEventListener('change', e => {
+    var val = e.target.value;
+    mainMusic.volume = val/100;
+    localStorage.setItem('sliderVal', val)
+    if(val == 0)
+        {
+            img_song1.setAttribute('src','assets/mute.png')
+            flag_mute_unmute = 0;
+        }
+    else
+    {
+        img_song1.setAttribute('src','assets/voice.png')
+        flag_mute_unmute = 1;
+    }
+
+});
+
+img_song1.addEventListener('click', e => {
+    
+    if(JSON.parse(localStorage.getItem('sliderVal')) == 0 && JSON.parse(localStorage.getItem('pre_sliderVal')) !=0)
+        {
+            img_song1.setAttribute('src','assets/mute.png')
+            localStorage.setItem('pre_sliderVal',slider_song1.value)
+            localStorage.setItem('sliderVal',0)
+            slider_song1.value = 0;
+            flag_mute_unmute=0;
+        }
+    else{
+        img_song1.setAttribute('src','assets/voice.png')
+        slider_song1.value = JSON.parse(localStorage.getItem('pre_sliderVal'));
+        localStorage.setItem('pre_sliderVal',0)
+        localStorage.setItem('sliderVal',slider_song1.value)
+        flag_mute_unmute = 1;
+    }
 
 });
